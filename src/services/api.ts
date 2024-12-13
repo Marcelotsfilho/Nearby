bf74df3b28cbefc2e18bc2c20c98685335562a7f
Marcelotsfiilho <marcelo.estudo.cefet@gmail.com>
2024-12-13 14:41:06 -0300

    Finalização do aplicativo móvel Nearby

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,6 +2,6 @@ import axios from "axios";
 
 export const api = axios.create({
   //base: protocolo http; ip gerado pelo expo no terminal; :porta em que o servidor esta rodando
-  baseURL: "http://192.168.1.7:3333",
+  baseURL: "http://200.18.141.84:3333",
   timeout: 700,
 })
